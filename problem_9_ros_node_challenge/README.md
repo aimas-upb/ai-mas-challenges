2a7d9554878d8bb1ba425e9e635b82e0694e7615
Alexandru Sorici <alex.sorici@gmail.com>
2018-10-11 19:00:26 +0300

    Problem 9 description

---
+++ problem_9_ros_node_challenge/README.md
@@ -0,0 +1,53 @@
+The ROS Node Challenge
+======================
+
+In robotics research and, more recently, also in Autonomous Driving, the
+**Robot Operating System** ([ROS](http://wiki.ros.org/)) framework is a
+de-facto standard for application development.
+
+ROS can be described as an advanced publish-subscribe message transport system.
+Applications can, for example, subscribe to measurement update messages that are published
+by _nodes_ managing individual sensors.
+Since each sensor can have its own _frame of reference_ for measurements, ROS sets up a
+mechanism for publishing _[transforms](http://wiki.ros.org/tf)_ which convert measurements
+(e.g. coordinates, distances, from one frame to another).
+
+Your challenge is to master the basic use of the ROS publish/subscribe mechanism and of
+transform writing.
+
+### Problem Description
+
+You are given two sensors that can measure _distances_ in 2D. The sensors report their findings in
+polar coordinates, i.e. a _distance_ value and an _angle_ (expressed in radians,
+measuring counterclockwise rotation from the X-axis of the sensor).
+
+However, the sensors are not perfectly accurate and can have measurement errors.
+
+You are given two CSV files, which contain 100 range measurements **of the same target**
+(i.e. a single point in 2D), one for each sensor.
+
+Your tasks are the following:
+
+  1. Analyse the measurement distributions for distance and angle for each sensor
+  2. Since they are distances and headings towards the same point, establish the 2D transformation
+    operations (rotation and translation) required to convert measurements _from_ the reference
+    frame of sensor 1 _to_ the reference frame of sensor 2.
+  3. Write the code for a [ROS Node](http://wiki.ros.org/ROS/Tutorials/UnderstandingNodes),
+  corresponding to sensor 1, which publishes the transform from the reference frame of sensor 1, to that of sensor 2.
+  The frame IDs should be named _sensor1_ and _sensor2_, respectively. Study
+  [this tutorial](http://wiki.ros.org/tf/Tutorials/Writing%20a%20tf%20broadcaster%20%28Python%29) to get started.
+
+  4. Have the the above ROS Node _publish_ a set of random range measurements in the polar coordinate
+  form mentioned previously (distance, angle in radians).
+  The measurements are published _in the reference frame_ of sensor 1. For this you have to:
+
+    - define a ROS message format for your measurements (see [this tutorial](http://wiki.ros.org/ROS/Tutorials/CreatingMsgAndSrv) for examples)
+    - _publish_ the measurements on the /range/sensor1 topic (see [this ROS Publsih/Subscribe tutorial](http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29)
+     to get started)
+
+  5. Write another ROS Node, whose job it is to:
+
+    - subscribe to the ROS Node for sensor 1 to receive range measurements of that node
+    - use to the transform broadcast by the ROS Node of sensor 1 to convert the measurements from the
+    reference frame of sensor 1 to that of sensor 2
+    - publish the transformed measurements under the topic /range/sensor2
---
+++ problem_9_ros_node_challenge/data_generator.py
@@ -0,0 +1,84 @@
+import numpy as np
+import math, csv
+import matplotlib.pyplot as plt
+
+DISTANCE = 10
+ANGLE = np.pi / 4;
+
+AXIS_ROT_ANGLE = -np.pi / 2;
+AXIS_TRANS = np.array([10, 0])
+
+MEAN_SENSOR = np.array([DISTANCE, ANGLE])
+COVARIANCE_SENSOR = np.array([[0.75 * 0.75, 0], [0, (np.pi / 90) * (np.pi / 90)]])
+
+if __name__ == "__main__":
+    samples_dist = np.random.multivariate_normal(MEAN_SENSOR, COVARIANCE_SENSOR, 100)
+
+    # plt.boxplot([180 * s[1] / np.pi for s in samples_dist])
+
+    samples_x = [d * math.cos(theta) for [d, theta] in samples_dist]
+    samples_y = [d * math.sin(theta) for [d, theta] in samples_dist]
+
+    samples_coord = [np.array(list(coords)) for coords in zip(samples_x, samples_y)]
+
+    rotation_matrix = np.array(
+        [[math.cos(AXIS_ROT_ANGLE), math.sin(AXIS_ROT_ANGLE)],
+         [-math.sin(AXIS_ROT_ANGLE), math.cos(AXIS_ROT_ANGLE)],
+        ]
+    )
+    trans_new_coord = np.matmul(AXIS_TRANS, rotation_matrix)
+
+    mean_sensor_x = DISTANCE * math.cos(ANGLE)
+    mean_sensor_y = DISTANCE * math.sin(ANGLE)
+
+    mean_sensor_rotated = np.matmul(np.array([mean_sensor_x, mean_sensor_y]), rotation_matrix)
+    mean_sensor_transformed = np.array([mean_sensor_rotated[0] - trans_new_coord[0],
+                                        mean_sensor_rotated[1] - trans_new_coord[1]]
+                                       )
+    mean_sensor_transformed_dist = np.array( [np.linalg.norm(mean_sensor_transformed),
+                                              math.atan2(mean_sensor_transformed[1], mean_sensor_transformed[0])]
+                                             )
+
+    samples_transformed_dist = np.random.multivariate_normal(mean_sensor_transformed_dist, COVARIANCE_SENSOR, 100)
+    plt.boxplot([180 * s[1] / np.pi for s in samples_transformed_dist])
+    plt.show()
+
+    # samples_rotated = [np.matmul(s, rotation_matrix) for s in samples_coord]
+    # samples_transformed_dist = [np.array([s[0] - trans_new_coord[0], s[1] - trans_new_coord[1]]) for s in samples_rotated]
+
+    # samples_transformed_x = [s[0] for s in samples_transformed_dist]
+    # samples_transformed_y = [s[1] for s in samples_transformed_dist]
+    #
+    # samples_transformed_dist = [np.array( [np.linalg.norm(s), math.atan2(s[1], s[0])] ) for s in samples_transformed_dist]
+    # plt.boxplot([180 * s[1] / np.pi for s in samples_transformed_dist])
+
+    # fig1, ax1 = plt.subplots()
+    # ax1.set_title("Points in original axis")
+    # ax1.scatter(samples_x, samples_y)
+    #
+    # fig2, ax2 = plt.subplots()
+    # ax2.set_title("Points in transformed axis")
+    # ax2.scatter(samples_transformed_x, samples_transformed_y)
+
+    fieldnames = ['distance', 'angle']
+
+    with open("data/sensor1.csv", mode='w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames= fieldnames)
+        writer.writeheader()
+
+        for s in samples_dist:
+            writer.writerow({
+                'distance': round(s[0], 4),
+                'angle': round(s[1], 4)
+            })
+
+    with open("data/sensor2.csv", mode='w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for s in samples_transformed_dist:
+            writer.writerow({
+                'distance': round(s[0], 4),
+                'angle': round(s[1], 4)
+            })
+
